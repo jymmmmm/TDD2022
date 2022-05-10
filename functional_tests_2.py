8621f297f007073b587dd505644169b49672ca94
jym <1416320489@qq.com>
2022-05-10 08:38:22 +0800

    Front page HTML now generated from a template

--- functional_tests_2.py
+++ functional_tests_2.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -7,9 +9,9 @@ class NewVisitorTest(unittest.TestCase):
         self.browser =webdriver.Chrome()
     def tearDown(self):
         self.browser.quit()
-    def test_can_start_a_list_and_reetrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heaed about a cool new online to-do app.She goes
-        #
+
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
@@ -18,15 +20,17 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
         )
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
         self.fail('Finish the test!')
 
