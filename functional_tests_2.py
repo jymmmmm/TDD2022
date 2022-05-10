7d596656bc75101a29b7e916f4880ac94d329ee4
jym <1416320489@qq.com>
2022-05-10 09:20:45 +0800

    重构前的提交

--- functional_tests_2.py
+++ functional_tests_2.py
@@ -23,15 +23,13 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
         self.fail('Finish the test!')
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -12,3 +12,8 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/',data={'item_text':'A new list item'})
+        self.assertIn('A new list item',response.content.decode())
+        self.assertTemplateUsed(response,'home.html')
+
